5b64ca4258eae3dd1516dad79cd29a3c8c58a62e
zgliu71 <zgliu71@a7780bf6-f67b-11dd-9fdb-3debd5008f28>
2009-02-17 04:47:08 +0000

    add tests

--- include/facet.h
+++ include/facet.h
@@ -8,20 +8,28 @@
 #include "line.h"
 #include "point.h"
 
+class Cadmodel;
+
 class Facet
 {
+    friend class Cadmodel;
 public:
     Facet(const Point& normal, const std::vector<Point> &pts);
     Facet(const Facet& rhs);
+    
     void scale(double factor);
     void changeDirection(const wxString& direction);
     std::pair<int, Line> intersect(double z);
-    Line intersect_0(double z); 
+   
+    static bool isIntersect(const Point& p1, const Point& p2, double z);
+    static Point calcIntersect(const Point& p1, const Point& p2, double z);
+    static double intersect(double x1, double y1, double x2, double y2, double x);
 
+private:
+    Line intersect_0(double z); 
     //
     Point normal;
     std::vector<Point> points;
-
 };
 
 #endif
